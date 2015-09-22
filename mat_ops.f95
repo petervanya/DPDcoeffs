2ae50be579cd1c43d6323ae102219a47154f2a22
petervanya <peter.vanya@gmail.com>
2015-09-22 13:30:27 +0100

    Initial commit

---
+++ mat_ops.f95
@@ -0,0 +1,17 @@
+subroutine get_pair_dist(mat, N, arr)
+! generate pairs of distances from an xyz matrix mat of size (N, 3)
+! 22/09/15
+  integer, intent(in) :: N
+  real, intent(in), dimension(N, 3) :: mat
+  real, intent(out), dimension(N*(N-1)/2) :: arr
+  integer :: i, j, cnt
+
+  cnt = 1
+  do i=1,N
+    do j=i+1,N
+      arr(i) = sqrt((mat(i,1)-mat(j,1))**2 + (mat(i,2)-mat(j,2))**2 + &
+      (mat(i,3)-mat(j,3))**2)
+      cnt = cnt + 1
+    enddo
+  enddo
+end subroutine
