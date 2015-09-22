7a81a53058d6acaee2a6fb6d39e4e34a2a47a653
petervanya <peter.vanya@gmail.com>
2015-09-22 13:32:04 +0100

    Fixed bugs, added Fortran routines

--- rdf.py
+++ rdf.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 """Usage:
-    rdf.py <dir> <fname> <binsize>
+    rdf.py <fnames> [--binsize <bs>] [--picname <pn>]
 
-Read all LAMMPS data files and compute the pair correlation function.
+Read all LAMMPS data files in the directory and compute the pair correlation function.
+Produce a histogram plot and save data into rdf.out.
+Uses Fortran routines from mat_ops.f95 produced with f2py.
 
 Arguments:
-    <dir>          Directory with output files
-    <fname>        Core of the filename, extension is 'xyz'
-    <binsize>      Size of the bins for the histogram [default: 0.05]
+    <fnames>          Core of the filename, extension is 'xyz'
+
+Options:
+    --binsize <bs>   Size of the bins for the histogram [default: 0.05]
+    --picname <pn>   Name of the histogram plot [default: hist.png]
 
 pv278@cam.ac.uk, 02/09/15
 """
@@ -16,38 +20,49 @@ import numpy as np
 from numpy.linalg import norm
 import matplotlib.pyplot as plt
 from math import pi
-import os
-import glob
+import os, glob, time
+import mat_ops
 
 def read_outfile(outfile):
-    """Read one xyz outfile into a np matrix"""
+    """Read one xyz outfile into a numpy matrix"""
     A = open(outfile, "r").readlines()[2:]
     A = [line.split() for line in A]
-    A = np.array(A).astype(float)
+    A = np.array(A, order="F").astype(float)
     return A
 
 def get_one_rdf(mat, dr=0.05):
-    """Compute pair correl'n fcn from matrix of positions"""
+    """Compute pair correl'n fcn from the xyz matrix"""
     N = len(mat)
     Npairs = N*(N-1)/2
-    d = []
-    for i in range(N):
-        for j in range(i+1, N):
-            d.append(norm(mat[i] - mat[j]))
-    bins = np.arange(0, 10+dr, dr)         # assume box size is 10
-    (rdf, r, c) = plt.hist(d, bins=bins)
-    r = r[:-1] + np.diff(r)/2
-    return r, rdf/(4*pi*r**2*dr)
+#    d = np.zeros(Npairs)
+#    here = 0
+#    for i in range(N):
+#        for j in range(i+1, N):
+#            d[here] = norm(mat[i] - mat[j])
+#            here += 1
+    d = mat_ops.get_pair_dist(mat)
 
-def get_master_rdf(outfiles):
-    Npairs = Nparts*(Nparts-1)/2
-    PCFmat = np.zeros((Npairs, Nfiles))
+    bins = np.arange(0, 10+dr, dr)         # assume box size is 10
+    (rdf_raw, r, c) = plt.hist(d, bins=bins)  # c is useless
+    r = r[:-1] + np.diff(r)/2
+    rdf = rdf_raw/(4*pi*r**2*dr)
+    return r, rdf
+
+
+def get_master_rdf(outfiles, N):
+    """Construct an rdf from all the available xyz files"""
+    Nfiles = len(outfiles)
+    Npairs = N*(N-1)/2
+#    rdf_mat = np.zeros((Npairs, Nfiles))
+    rdf_mat = []
     for i in range(Nfiles):
         A = read_outfile(outfiles[i])
-        r, PCFmat[:, i] = get_one_rdf(A[:, 1:])
+        r, rdf_i = get_one_rdf(A[:, 1:])
+        rdf_mat.append(rdf_i)
         print outfiles[i], "done"
-    rdf = np.sum(PCFmat, 1)/len(outfiles)
+    rdf = np.sum(np.array(rdf_mat), 0)/len(outfiles)
     return r, rdf
+
 
 def save_data(vec1, vec2, outfile):
     with open(outfile, "w") as f:
@@ -58,20 +73,20 @@ def save_data(vec1, vec2, outfile):
 if __name__ == "__main__":
     args = docopt(__doc__)
 #    print args
-    dr = float(args["<binsize>"])
-    outfiles = glob.glob(args["<dir>"] + "/" + args["<fname>"] + "*.xyz")
+    dr = float(args["--binsize"])
+    outfiles = glob.glob(args["<fnames>"])
+    print outfiles
     Nfiles = len(outfiles)
-    Nparts = int(open(outfiles[0], "r").readline())
-    print "Particles:", Nparts
-
-    A = read_outfile(outfiles[0])
-    r, vals = get_one_rdf(A[:, 1:], dr=dr)
-    print outfiles[0]
-#    rdf = get_master_rdf(outfiles)
+    N = int(open(outfiles[0], "r").readline())
+    print "Particles:", N
+    
+    r, vals = get_master_rdf(outfiles, N)
+#    A = read_outfile(outfiles[0])
+#    r, vals = get_one_rdf(A[:, 1:], dr=dr)
 
     plt.cla()
     plt.plot(r, vals)
-    plt.savefig("hist.png")
+    plt.savefig(args["--picname"])
     save_data(r, vals, "rdf.out")
 
 
