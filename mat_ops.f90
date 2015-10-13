2ff4346d93c4fb8f21478d6fb2cddbf46b5e9f63
petervanya <peter.vanya@gmail.com>
2015-10-13 01:02:26 +0100

    Added new function

--- mat_ops.f90
+++ mat_ops.f90
@@ -1,17 +1,38 @@
 subroutine get_pair_dist(mat, N, arr)
 ! generate pairs of distances from an xyz matrix mat of size (N, 3)
 ! 22/09/15
-  integer, intent(in) :: N
-  real, intent(in), dimension(N, 3) :: mat
-  real, intent(out), dimension(N*(N-1)/2) :: arr
-  integer :: i, j, cnt
+    integer, intent(in) :: N
+    real, intent(in), dimension(N, 3) :: mat
+    real, intent(out), dimension(N*(N-1)/2) :: arr
+    integer :: i, j, cnt
 
-  cnt = 1
-  do i = 1, N
-    do j = i+1, N
-      arr(cnt) = sqrt((mat(i, 1)-mat(j, 1))**2 + (mat(i, 2)-mat(j, 2))**2 + &
+    cnt = 1
+    do i = 1, N
+        do j = i+1, N
+            arr(cnt) = sqrt((mat(i, 1)-mat(j, 1))**2 + (mat(i, 2)-mat(j, 2))**2 + &
                       (mat(i, 3)-mat(j, 3))**2)
-      cnt = cnt + 1
+            cnt = cnt + 1
+        enddo
     enddo
-  enddo
+end subroutine
+
+
+subroutine get_pair_dist2(mat, N, arr)
+! generate pairs of cistances together with the particle types
+! 11/10/15
+    integer, intent(in) :: N
+    real, intent(in), dimension(N, 4) :: mat
+    real, intent(out), dimension(N*(N-1)/2, 3) :: arr
+    integer :: i, j, cnt
+
+    cnt = 1
+    do i = 1, N
+        do j = i+1, N
+            arr(cnt, 1) = mat(i, 1)
+            arr(cnt, 2) = mat(j, 1)
+            arr(cnt, 3) = sqrt((mat(i, 2)-mat(j, 2))**2 + (mat(i, 3)-mat(j, 3))**2 + &
+                          (mat(i, 4)-mat(j, 4))**2)
+            cnt = cnt + 1
+        enddo
+    enddo
 end subroutine
