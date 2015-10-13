1423e88a048da2ea1599e3556153f0c5dadd7ef8
petervanya <peter.vanya@gmail.com>
2015-10-13 14:48:48 +0100

    Added function to calculate order param

--- mat_ops.f90
+++ mat_ops.f90
@@ -36,3 +36,41 @@ subroutine get_pair_dist2(mat, N, arr)
         enddo
     enddo
 end subroutine
+
+subroutine get_local_op(xyz, N, rc, phi)
+!
+! 12/10/15
+    integer, intent(in) :: N
+    real, intent(in) :: rc
+    real, intent(in), dimension(N, 4) :: xyz
+    real, intent(out) :: phi
+    integer, dimension(N-1) :: na, nb
+    integer :: i, j
+
+    na = 0
+    nb = 0
+    phi = 0.0
+
+    do i = 1, N
+        do j = i+1, N
+            dist = sqrt((xyz(i, 2)-xyz(j, 2))**2 + (xyz(i, 3)-xyz(j, 3))**2 + &
+                          (xyz(i, 4)-xyz(j, 4))**2)
+            if (dist < rc) then
+                if (xyz(i, 1) == 1) then
+                    na(i) = na(i) + 1
+                    na(j) = na(j) + 1
+                else if (xyz(i, 1) == 2) then
+                    nb(i) = nb(i) + 1
+                    nb(j) = nb(j) + 1
+                endif
+            endif
+        enddo
+    enddo
+    
+    do i = 1, N-1
+        phi = phi + float(na(i) - nb(i))**2/(na(i) + nb(i))**2
+    enddo
+    phi = phi/N
+end subroutine
+
+
