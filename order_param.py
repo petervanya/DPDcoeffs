8e0c933438fbc78f6266f48daf4c1263accf2123
petervanya <peter.vanya@gmail.com>
2015-10-14 12:46:57 +0100

    Major rework

--- order_param.py
+++ order_param.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """Usage:
-    order_param.py calc <systems> <rc> [--save <fname>]
+    order_param.py calc <systems> <rc> [--save <fname>] [--print]
     order_param.py plot <file>
 
 Calculate the order parameter for a binary mixture phase transition
@@ -24,12 +24,22 @@ from docopt import docopt
 import mat_ops
 
 
-def read_outfile(outfile):
-    """Read a LAMMPS xyz outfile into a numpy matrix"""
-    A = open(outfile, "r").readlines()[2:]
+def read_dumpfile(dumpfile):
+    """Read a LAMMPS xyz dumpfile into a numpy matrix"""
+    A = open(dumpfile, "r").readlines()[2:]
     A = [line.split() for line in A]
     A = np.array(A, order="F").astype(float)
     return A
+
+
+def save_matrix(mat, fname):
+    m, n = mat.shape
+    with open(fname, "w") as f:
+        for i in range(m):
+            for j in range(n):
+                f.write(str(mat[i, j]) + "\t")
+            f.write("\n")
+    print "Matrix written in file", fname
 
 
 def get_op(xyz_mat, rc):
@@ -46,65 +56,56 @@ def get_op(xyz_mat, rc):
     return float(AB)/(AA+BB)
 
 
-def get_average_op(outfiles, rc):
+def get_average_op(dumpfiles, rc, fA):
     """From given xyz files extract average order param"""
     order_params = []
-    for outfile in outfiles:
-        A = read_outfile(outfile)
-        order_params.append(get_op(A, rc))
+    for dumpfile in dumpfiles:
+        A = read_dumpfile(dumpfile)
+#        order_params.append(get_op(A, rc))                # common sense, NOT GOOD
+#        order_params.append(mat_ops.get_local_op(A, rc))   # Goyal thesis, calling Fortran
+        n1, n2 = mat_ops.get_local_op2(A, rc)               # Goyal thesis, alternative call
+        n1 = n1/fA
+        n2 = n2/(1-fA)
+        op = float(np.dot(n1-n2, n1-n2))/np.dot(n1+n2, n1+n2)
+        order_params.append(op)
     return np.average(order_params)
 
 
-def savemat(mat, fname):
-    m, n = mat.shape
-    with open(fname, "w") as f:
-        for i in range(m):
-            for j in range(n):
-                f.write(str(mat[i, j]) + "\t")
-            f.write("\n")
-    print "Matrix written in file", fname
-
-
-def pipeline_old(outfiles, rc):
+def pipeline_old(dumpfiles, rc):
     """(Old working of this script)
-    From the outfiles extract the order param and print it"""
-    print get_average_op(outfiles, rc)
+    From the dumpfiles extract the order param and print it"""
+    print get_average_op(dumpfiles, rc)
 
 
 if __name__ == "__main__":
     args = docopt(__doc__)
-    print args
+#    print args
 
     if args["calc"]:
         rc = float(args["<rc>"])
-##       outfiles = glob.glob(args["<files>"])
         systems = glob.glob(args["<systems>"])
-##       print systems
         fAs = list(set([float(system.split("_")[1]) for system in systems]))
         ABs = list(set([float(system.split("_")[3]) for system in systems]))
         fAs.sort()
         ABs.sort()
-        print fAs, "\n", np.array([ABs]).T
+        print fAs, "\n", ABs
  
         op_mat = np.zeros((len(ABs), len(fAs)))
         for i in range(len(ABs)):
             for j in range(len(fAs)):
-##               print fAs[j], ABs[i]
                 system = "fA_" + str(fAs[j]) + "_AB_" + str(ABs[i])
                 path = os.path.join(os.path.expanduser("~/DPDcoeffs/BinMixt"), system, "Dump/dump*00.xyz")
                 xyzfiles = glob.glob(path)
-                op_mat[i, j] = get_average_op(xyzfiles, rc)
- 
-        if args["--save"]:
-            savemat(op_mat, args["--save"])
-        else:
+                op_mat[i, j] = get_average_op(xyzfiles, rc, fAs[j])
+        save_matrix(op_mat, args["--save"])
+        if args["--print"]:
             print op_mat
 
     elif args["plot"]:
         data = np.loadtxt(args["<file>"])
         fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
-        Axed3D.plot_surface(data)
+        Axes3D.plot_surface(data)
         plt.show()
 
 
