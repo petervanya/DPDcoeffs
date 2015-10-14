7ec95dc7db96b471a8b3992f35633c4076b88974
petervanya <peter.vanya@gmail.com>
2015-10-14 12:41:39 +0100

    Improved function for order param

--- mat_ops.f90
+++ mat_ops.f90
@@ -38,39 +38,88 @@ subroutine get_pair_dist2(mat, N, arr)
 end subroutine
 
 subroutine get_local_op(xyz, N, rc, phi)
-!
+! Calculate and return order parameter (according to Goyal PhD thesis)
 ! 12/10/15
     integer, intent(in) :: N
     real, intent(in) :: rc
     real, intent(in), dimension(N, 4) :: xyz
     real, intent(out) :: phi
-    integer, dimension(N-1) :: na, nb
+    integer, dimension(N) :: n1, n2
     integer :: i, j
-
-    na = 0
-    nb = 0
+    
+    n1 = 0
+    n2 = 0
     phi = 0.0
 
     do i = 1, N
         do j = i+1, N
             dist = sqrt((xyz(i, 2)-xyz(j, 2))**2 + (xyz(i, 3)-xyz(j, 3))**2 + &
-                          (xyz(i, 4)-xyz(j, 4))**2)
+                        (xyz(i, 4)-xyz(j, 4))**2)
             if (dist < rc) then
-                if (xyz(i, 1) == 1) then
-                    na(i) = na(i) + 1
-                    na(j) = na(j) + 1
-                else if (xyz(i, 1) == 2) then
-                    nb(i) = nb(i) + 1
-                    nb(j) = nb(j) + 1
+                if (xyz(j, 1) == 1) then   ! neighbour of i-th particle is type 1
+                    n1(i) = n1(i) + 1
+                endif
+                if (xyz(j, 1) == 2) then   ! neighbour of i-th particle is type 2
+                    n2(i) = n2(i) + 1
+                endif
+                if (xyz(i, 1) == 1) then   ! vice versa
+                    n1(j) = n1(j) + 1
+                endif
+                if (xyz(i, 1) == 2) then   ! vice versa
+                    n2(j) = n2(j) + 1
                 endif
             endif
         enddo
     enddo
     
-    do i = 1, N-1
-        phi = phi + float(na(i) - nb(i))**2/(na(i) + nb(i))**2
+    do i = 1, N
+        phi = phi + float(n1(i) - n2(i))**2/(n1(i) + n2(i))**2
+!        if (n1(i) == 0) then 
+!            print *, "n1", i
+!        endif
+!        if (n2(i) == 0) then
+!            print *, "n2", i
+!        endif 
     enddo
+!    print *, phi
     phi = phi/N
+end subroutine
+
+
+subroutine get_local_op2(xyz, N, rc, n1, n2)
+! Return two nearest neighbour occupation arrays n1, n2 to later calculate 
+! the order parameter (according to Goyal PhD thesis)
+! 12/10/15
+    integer, intent(in) :: N
+    real, intent(in) :: rc
+    real, intent(in), dimension(N, 4) :: xyz
+    integer, intent(out), dimension(N) :: n1, n2
+    integer :: i, j
+    
+    n1 = 0
+    n2 = 0
+    phi = 0.0
+
+    do i = 1, N
+        do j = i+1, N
+            dist = sqrt((xyz(i, 2)-xyz(j, 2))**2 + (xyz(i, 3)-xyz(j, 3))**2 + &
+                        (xyz(i, 4)-xyz(j, 4))**2)
+            if (dist < rc) then
+                if (xyz(j, 1) == 1) then   ! neighbour of i-th particle is type 1
+                    n1(i) = n1(i) + 1
+                endif
+                if (xyz(j, 1) == 2) then   ! neighbour of i-th particle is type 2
+                    n2(i) = n2(i) + 1
+                endif
+                if (xyz(i, 1) == 1) then   ! vice versa
+                    n1(j) = n1(j) + 1
+                endif
+                if (xyz(i, 1) == 2) then   ! vice versa
+                    n2(j) = n2(j) + 1
+                endif
+            endif
+        enddo
+    enddo
 end subroutine
 
 
