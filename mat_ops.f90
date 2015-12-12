3f85688f5192122e5134e4764a5ba242aa9fa073
petervanya <peter.vanya@gmail.com>
2015-12-12 00:28:12 +0000

    Polish

--- mat_ops.f90
+++ mat_ops.f90
@@ -1,16 +1,17 @@
 subroutine get_pair_dist(mat, N, arr)
 ! generate pairs of distances from an xyz matrix mat of size (N, 3)
 ! 22/09/15
-    integer, intent(in) :: N
-    real, intent(in), dimension(N, 3) :: mat
-    real, intent(out), dimension(N*(N-1)/2) :: arr
+    integer, parameter :: k = selected_int_kind(16)
+    integer(kind=k), intent(in) :: N
+    real(8), intent(in), dimension(N, 3) :: mat
+    real(8), intent(out), dimension(N*(N-1)/2) :: arr
     integer :: i, j, cnt
+    ! f2py depend(N) mat, arr
 
     cnt = 1
     do i = 1, N
         do j = i+1, N
-            arr(cnt) = sqrt((mat(i, 1)-mat(j, 1))**2 + (mat(i, 2)-mat(j, 2))**2 + &
-                      (mat(i, 3)-mat(j, 3))**2)
+            arr(cnt) = sqrt(sum((mat(i, :) - mat(j, :))**2))
             cnt = cnt + 1
         enddo
     enddo
@@ -121,5 +122,4 @@ subroutine get_local_op2(xyz, N, rc, n1, n2)
         enddo
     enddo
 end subroutine
-
 
