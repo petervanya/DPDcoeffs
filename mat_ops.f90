4255cae614e84ea39b7643d760fb853f706fea63
petervanya <peter.vanya@gmail.com>
2016-03-15 15:50:32 +0000

    Added functions

--- mat_ops.f90
+++ mat_ops.f90
@@ -1,16 +1,20 @@
-subroutine get_pair_dist(mat, N, arr)
-! generate pairs of distances from an xyz matrix mat of size (N, 3)
+!module mat_ops
+!contains
+! CANNOT USE module ... contains ... end module STRUCTURE WITH f2py
+
+subroutine get_pair_dist(mat, n, arr)
+! generate pairs of distances from an xyz matrix mat of size (n, 3)
 ! 22/09/15
     integer, parameter :: k = selected_int_kind(16)
-    integer(kind=k), intent(in) :: N
-    real(8), intent(in), dimension(N, 3) :: mat
-    real(8), intent(out), dimension(N*(N-1)/2) :: arr
+    integer(kind=k), intent(in) :: n
+    real(8), intent(in), dimension(n, 3) :: mat
+    real(8), intent(out), dimension(n*(n-1)/2) :: arr
     integer :: i, j, cnt
-    ! f2py depend(N) mat, arr
+    ! f2py depend(n) mat, arr
 
     cnt = 1
-    do i = 1, N
-        do j = i+1, N
+    do i = 1, n
+        do j = i+1, n
             arr(cnt) = sqrt(sum((mat(i, :) - mat(j, :))**2))
             cnt = cnt + 1
         enddo
@@ -19,7 +23,7 @@ end subroutine
 
 
 subroutine get_pair_dist2(mat, N, arr)
-! generate pairs of cistances together with the particle types
+! generate pairs of distances together with the particle types
 ! 11/10/15
     integer, intent(in) :: N
     real, intent(in), dimension(N, 4) :: mat
@@ -123,3 +127,86 @@ subroutine get_local_op2(xyz, N, rc, n1, n2)
     enddo
 end subroutine
 
+
+function get_n_bins(a, n, dr) result (n_bins)
+    integer, intent(in) :: n
+    real(8), intent(in) :: a(n)
+    real(8), intent(in) :: dr
+    integer :: n_bins
+    n_bins = int((maxval(a) - minval(a))/dr) + 1
+end function
+
+
+pure function n_bins(a, n, dr)
+! Return the number of bins based on min and max value of a and bin size dr
+    integer, intent(in) :: n
+    real(8), intent(in) :: a(n)
+    real(8), intent(in) :: dr
+    integer :: n_bins
+    n_bins = int((maxval(a) - minval(a))/dr) + 1
+end function
+
+
+!subroutine create_hist_gnu(a, n, dr, bins, hist)
+!! Create histogram with bins of size dr, version for gfortran
+!    integer, intent(in) :: n
+!    real(8), intent(in) :: a(n)
+!    real(8), intent(in) :: dr
+!    integer, intent(out), allocatable :: hist(:)
+!    real(8), intent(out), allocatable :: bins(:)
+!    integer :: i, j
+!    
+!    n_b = n_bins(a, n, dr)
+!    allocate(bins(n_b+1))
+!    allocate(hist(n_b))
+!    hist = 0
+!    
+!    ! Create bins
+!    bins(1) = minval(a)
+!    do i = 1, n_b
+!        bins(i+1) = bins(i) + dr
+!    enddo
+!    print *, bins
+!    print *, hist
+!    
+!    ! Create the histogram
+!    do j = 1, n
+!        do i = 1, n_b
+!            if (a(j) > bins(i) .and. a(j) < bins(i+1)) then ! CHECK
+!                hist(i) = hist(i) + 1
+!                exit
+!            endif
+!        enddo
+!    enddo
+!end subroutine
+
+
+subroutine create_hist(a, n, n_b, hist, bins)
+! Create histogram with bins of size dr, version for f2py
+    integer, intent(in) :: n
+    integer, intent(in) :: n_b
+    real(8), intent(in) :: a(n)
+    real(8) :: dr
+    real(8), intent(out) :: bins(n_b+1)! bins(int(sqrt(real(n)))+2) !(n_bins(a, n, dr)+1)
+    integer, intent(out) :: hist(n_b)! hist(int(sqrt(real(n)))+1) !(n_bins(a, n, dr))
+    integer :: i, j
+    hist = 0
+    
+    bins(1) = minval(a)
+    dr = (maxval(a) - minval(a))/n_b
+    do i = 1, n_b
+        bins(i+1) = bins(i) + dr
+    enddo
+    
+    ! Create the histogram
+    do j = 1, size(a)
+        do i = 1, n_b
+            if (a(j) > bins(i) .and. a(j) < bins(i+1)) then ! CHECK
+                hist(i) = hist(i) + 1
+                exit
+            endif
+        enddo
+    enddo
+end subroutine
+
+!end module
