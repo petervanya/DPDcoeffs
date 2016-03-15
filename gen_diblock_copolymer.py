4f0de76976118f72e1d1ced361133d9a639606c0
petervanya <peter.vanya@gmail.com>
2016-03-15 17:48:13 +0000

    Major rework; new UI

--- gen_diblock_copolymer.py
+++ gen_diblock_copolymer.py
@@ -1,141 +1,115 @@
 #!/usr/bin/env python
 """Usage:
-    gen_polymer.py <N> <f> [--dist <a>] [--FAA <AA>] [--FAB <AB>] 
-                           [--seed <s>] [--boxsize <L>] [--save <fname>] 
+    gen_polymer.py <N> <f> [--L <L>] [--rho <rho>] [--aii <aii>] [--aij <aij>]
+                           [--save <fname>] 
 
 Generate a binary mixture from A and B monomers: AA...ABB...B
-UNFINISHED
-LAMMPS atom_style molecular
 
 Arguments:
-    <N>              Polymerisation (# of monomers)
-    <f>              fraction of A monomers
+    <N>              Polymerisation (number of monomers in chain)
+    <f>              fraction of A monomers in chain
 
 Options:
-    --FAA <AA>       Force between AA (and BB) beads [default: 1.0]
-    --FAB <AB>       Force between AB beads [default: 1.0]
-    --dist <a>       Initial distance between successive beads [default: 0.1]
-    --save <fname>   Save into file
-    --seed <s>       Random seed [default: 123]
-    --boxsize <L>    Size of the simulation box L**3 [default: 10]
+    --L <L>          Size of the simulation box L**3 [default: 10]
+    --rho <rho>      Density of the system [default: 3.0]
+    --aii <aii>      Coefficient between same beads [default: 15.0]
+    --aij <aij>      Coefficient between different beads [default: 21.0]
+    --save <fname>   Output save file
 
-pv278@cam.ac.uk, 28/09/15
+pv278@cam.ac.uk, 15/03/16
 """
 import numpy as np
+from math import *
 from docopt import docopt
-from xyzlib import Atoms
+import lmp_lib as ll
 
 
-def get_copoly_xyz(N, f, a, start=np.zeros(3), angles=[0, 0], sigma=0.1):
-    """
-    Generate the xyz matrix of the polymer with coordinates 
-    wiggled by gaussian noise.
-    Atom_style molecular
-    * sigma -- standard deviation for the noise
-    """
-    NA = int(f*N)
-    types = [1]*NA + [2]*(N-NA)
-    coords = np.hstack(( np.zeros((N, 2)), np.matrix(np.linspace(0, (N-1)*a, N)).T ))  # z-coord
-    xyz = Atoms(names=[], coords=coords)
-    xyz.rotate(angles[0], angles[1])           # rotate the polymer
-    xyz.coords += np.random.randn(N, 3)*0.01    # gaussian noise
-#    xyz.coords[xyz.coords<0.0] = 0.0           # return leaking atoms to the box
-    xyz.coords += start                        # shift
-    xyz_mat = np.hstack((np.arange(1, N+1, dtype=int).reshape((N, 1)), \
-                         np.ones((N, 1), dtype=int), np.ones((N, 1), dtype=int), \
-                         xyz.coords))
-    xyz_mat[:, 0].astype(int)
-    return xyz_mat
+def grow_polymer(L, f, n, Nc, mu=1.0, sigma=0.1):
+    """Generate coordinates of matrix polymer chains (taken from gen_pmma.py)
+    return (5, n*Nc) matrix, columns: [mol_ids, bead_type, xyz]
+    Input:
+    * L: cubic box size
+    * n: polymerisation
+    * Nc: number of chains
+    * mu: mean of bead distance
+    * sigma: deviation of bead distance"""
+    xyz = np.zeros((Nc*n, 5))
+    atom_ids = np.matrix( [1]*int(n*f) + [2]*int(n*(1-f)) ).T
+    for i in range(Nc):
+        xyz[i*n : (i+1)*n] = np.hstack(( (i+1)*np.matrix(np.ones(n)).T,\
+                                         atom_ids,\
+                                         grow_one_chain(L, n, Nc, mu, sigma) ))
+    return xyz
 
 
-def get_copoly_bonds(N):
-    """Generate the bond matrix: [num, type, part1, part2]
-    Stiffness constants (type) are the same"""
-    bond_mat = np.vstack((np.arange(1, N), np.ones(N-1, dtype=int), \
-                          np.arange(1, N), np.arange(2, N+1) )).T
-    return bond_mat
+def grow_one_chain(L, n, Nc, mu, sigma):
+    """Return (3,n) xyz matrix of one chain (taken from gen_pmma.py)"""
+    xyz = np.zeros((n, 3))
+    xyz[0] = np.random.rand(3)*L
+    for i in range(1, n):
+        theta = np.random.rand()*pi
+        phi = np.random.rand()*2*pi
+        r = mu + np.random.randn()*L*sigma
+        new_bead_pos = [r*cos(theta), r*sin(theta)*cos(phi), r*sin(theta)*sin(phi)]
+        xyz[i] = xyz[i-1] + new_bead_pos
+        xyz[i] = np.where(xyz[i] > L, L, xyz[i])       # set coord to L or 0 on the boundary
+        xyz[i] = np.where(xyz[i] < 0.0, 0.0, xyz[i])
+    return xyz
 
 
-def get_header(xyz_mat, bond_mat, ang_mat=np.array([]), \
-               dih_mat=np.array([]), imp_mat=np.array([])):
-    """Generate LAMMPS header"""
-    s = "#blabla\n"
-    s += str(len(xyz_mat)) + " atoms\n"
-    s += str(len(bond_mat)) + " bonds\n"
-    s += str(len(set(xyz_mat[:, 1]))) + " atom types\n"   # select unique
-    s += str(len(set(bond_mat[:, 1]))) + " bond types\n"
-    s += "\n"
-    s += "0.0 " + str(L) + " xlo xhi\n"
-    s += "0.0 " + str(L) + " ylo yhi\n"
-    s += "0.0 " + str(L) + " zlo zhi\n"
-    s += "\n"
-    return s
-
-
-def get_masses(id_mass):
-    """Save list of masses in a string"""
-    s = "Masses\n\n"
-    for k, v in id_mass.iteritems():
-        s += str(k) + "\t" + str(v) + "\n"
-    return s
-
-
-def get_bond_coeffs(id_bondcoeffs):
-    """Save bond coefficients into a string"""
-    s = "Bond Coeffs\n\n"
-    for k, v in id_bondcoeffs.iteritems():
-        str_v = ""
-        for i in len(v):
-            str_v += str(i) + "\t"
-        s += str(k) + "\t" + str_v + "\n"
-    return s
-
-
-def atoms_to_str(mat):
-    """Convert atomic matrix to string"""
-    s = ""
-    M, N = mat.shape
-    for i in range(M):
-        # number, type, mass, x, y, z
-        s += "%i\t%i\t%.f\t%.6f\t%.6f\t%.6f\n" % tuple(mat[i])
-    return s
-
-
-def mat_to_str(mat):
-    """Convert matrix to string"""
-    s = ""
-    M, N = mat.shape
-    for i in range(M):
-        for j in range(N):
-            s += str(mat[i, j]) + "\t"
-        s += "\n"
-    return s
+def gen_bonds(n, Nc):
+    """Create bond matrix (taken from gen_pmma.py)
+    return (n*Nc, 3) matrix, columns: [bond_type, atom1, atom2]
+    Input:
+    * n: polymerisation
+    * Nc: number of chains"""
+    mat = np.zeros(((n-1)*Nc, 3), dtype=int)
+    for i in range(Nc):
+        one_chain_bonds = np.hstack(( np.matrix([1]*(n-1)).T,\
+                                      np.matrix( np.arange(n*i+1, n*(i+1)) ).T,\
+                                      np.matrix( np.arange(n*i+2, n*(i+1)+1) ).T ))
+        mat[i*(n-1) : (i+1)*(n-1)] = one_chain_bonds
+    return mat
 
 
 if __name__ == "__main__":
     args = docopt(__doc__)
-#    print args
     N = int(args["<N>"])
     f = float(args["<f>"])
-    a = float(args["--dist"])
-    L = float(args["--boxsize"])
-    FAA = float(args["--FAA"])
-    FAB = float(args["--FAB"])
+    L = float(args["--L"])
+    rho = float(args["--rho"])
+    a_ii = float(args["--aii"])
+    a_ij = float(args["--aij"])
     fname = args["--save"]
-    seed = int(args["--seed"])
-    np.random.seed(seed)
+    np.random.seed(1234)
+
+    Nb = rho * L**3
+    Nc = int(Nb/N)
+    rc = 1.0
     
-    xyz_mat = get_copoly_xyz(N, f, a, start=[2.0, 2.0, 0.0]) 
-    bond_mat = get_copoly_bonds(N)
+    print "=== Creating LAMMPS data file for diblock copolymer melt ==="
+    print "Set interaction params in the input file"
+    print "Box size:", L, "| Density:", rho, "| Polymerisation:", N, "| A beads fraction:", f
+    
+    poly_xyz = grow_polymer(L, f, N, Nc, mu=rc/2, sigma=rc/10)  # SET mu CAREFULLY
+    xyz_str = ll.atoms2str(poly_xyz)
+    print len(poly_xyz), "beads created, density:", len(poly_xyz)/L**3
 
-    final_string = get_header(xyz_mat, bond_mat) + \
-                   get_masses({1: 1.0}) + \
-                   "\nAtoms\n\n" + atoms_to_str(xyz_mat) + \
-                   "\nBonds\n\n" + mat_to_str(bond_mat)
+    bonds = gen_bonds(N, Nc)
+    bonds_str = ll.bonds2str(bonds)
+    print len(bonds), "bonds created"
 
+    final_string = ll.header2str(len(poly_xyz), len(bonds), 2, 1, L) + \
+                   ll.mass2str({1: 1.0, 2: 1.0}) + \
+                   "\nAtoms\n\n" + xyz_str +  \
+                   "\nBonds\n\n" + bonds_str
+    
     if args["--save"]:
         fname = args["--save"]
         open(fname, "w").write(final_string)
         print "Data file written in", fname
     else:
         print final_string
+
+
+
