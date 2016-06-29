ed8418931299f3ac8921383d5f22dfab0c9884a0
Peter Vanya <peter.vanya@gmail.com>
2016-06-29 13:54:28 +0100

    Major polish

--- gen_diblock_copolymer.py
+++ gen_diblock_copolymer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """Usage:
-    gen_polymer.py <N> <f> [--L <L> --rho <rho> --save <fname> --xyz <xyz>]
+    gen_diblock_copolymer.py <N> <f> [--L <L> --rho <rho> --xyz <xyz>]
 
 Generate a binary mixture from A and B monomers: AA...ABB...B
 
@@ -10,11 +10,10 @@ Arguments:
 
 Options:
     --L <L>          Size of the simulation box L**3 [default: 10]
-    --rho <rho>      Density of the system [default: 5.0]
-    --save <fname>   Output save file [default: diblock.data]
+    --rho <rho>      Density of the system [default: 3.0]
     --xyz <xyz>      Create xyz file
 
-pv278@cam.ac.uk, 15/03/16
+pv278@cam.ac.uk, 29/06/16
 """
 import numpy as np
 from math import *
@@ -22,36 +21,34 @@ from docopt import docopt
 import lmp_lib as ll
 
 
-def grow_polymer(L, f, n, Nc, mu=1.0, sigma=0.1):
+def grow_polymer(L, f, N, Nc, mu=1.0, sigma=0.1):
     """Generate coordinates of matrix polymer chains (taken from gen_pmma.py)
     return (n*Nc, 5) matrix, columns: [mol_ids, bead_type, xyz]
     Input:
     * L: cubic box size
-    * n: polymerisation
+    * N: polymerisation
     * Nc: number of chains
     * mu: mean of bead distance
     * sigma: deviation of bead distance"""
-    xyz = np.zeros((Nc*n, 5))
-    atom_ids = np.matrix( [1]*int(n*f) + [2]*int(n*(1-f)) ).T
+    xyz = np.zeros((Nc*N, 5))
+    atom_ids = np.matrix( [1]*int(N*f) + [2]*(N-int(N*f)) ).T
     for i in range(Nc):
-        xyz[i*n : (i+1)*n] = np.hstack(( (i+1)*np.matrix(np.ones(n)).T,\
+        xyz[i*N : (i+1)*N] = np.hstack(( (i+1)*np.matrix(np.ones(N)).T,\
                                          atom_ids,\
-                                         grow_one_chain(L, n, Nc, mu, sigma) ))
+                                         grow_one_chain(L, N, Nc, mu) ))
     return xyz
 
 
-def grow_one_chain(L, n, Nc, mu, sigma):
+def grow_one_chain(L, n, Nc, mu=1.0):
     """Return (n, 3) xyz matrix of one chain (taken from gen_pmma.py)"""
     xyz = np.zeros((n, 3))
     xyz[0] = np.random.rand(3)*L
     for i in range(1, n):
-        theta = np.random.rand()*pi
+        th = np.random.rand()*pi
         phi = np.random.rand()*2*pi
-        r = mu + np.random.randn()*L*sigma
-        new_bead_pos = [r*cos(theta), r*sin(theta)*cos(phi), r*sin(theta)*sin(phi)]
+        r = mu
+        new_bead_pos = [r*cos(th), r*sin(th)*cos(phi), r*sin(th)*sin(phi)]
         xyz[i] = xyz[i-1] + new_bead_pos
-#        xyz[i] = np.where(xyz[i] > L, L, xyz[i])       # set coord to L or 0 on the boundary
-#        xyz[i] = np.where(xyz[i] < 0.0, 0.0, xyz[i])
     return xyz
 
 
@@ -63,9 +60,10 @@ def gen_bonds(n, Nc):
     * Nc: number of chains"""
     mat = np.zeros(((n-1)*Nc, 3), dtype=int)
     for i in range(Nc):
-        one_chain_bonds = np.hstack(( np.matrix([1]*(n-1)).T,\
-                                      np.matrix( np.arange(n*i+1, n*(i+1)) ).T,\
-                                      np.matrix( np.arange(n*i+2, n*(i+1)+1) ).T ))
+        one_chain_bonds = np.hstack((\
+                          np.matrix([1]*(n-1)).T,\
+                          np.matrix( np.arange(n*i+1, n*(i+1)) ).T,\
+                          np.matrix( np.arange(n*i+2, n*(i+1)+1) ).T ))
         mat[i*(n-1) : (i+1)*(n-1)] = one_chain_bonds
     return mat
 
@@ -81,13 +79,14 @@ if __name__ == "__main__":
     Nb = int(rho * L**3)
     Nc = int(Nb/N)
     rc = 1.0
-    mu, sigma = rc/2.0, rc/10.0
+    mu = rc/2.0
     
     print("=== Creating LAMMPS data file for diblock copolymer melt ===")
     print("Set interaction params in the input file")
-    print("Box size:", L, "| Density:", rho, "| Polymerisation:", N, "| A beads fraction:", f)
+    print("Box: %.1f | Rho: %.1f | Chain length: %i | A beads/chain: %i" % \
+          (L, rho, N, int(N*f) ))
     
-    poly_xyz = grow_polymer(L, f, N, Nc, mu, sigma)  # SET mu CAREFULLY
+    poly_xyz = grow_polymer(L, f, N, Nc, mu)
     xyz_str = ll.atoms2str(poly_xyz)
     print(len(poly_xyz), "beads created, density:", len(poly_xyz)/L**3)
 
@@ -100,7 +99,7 @@ if __name__ == "__main__":
                    "\nAtoms\n\n" + xyz_str +  \
                    "\nBonds\n\n" + bonds_str
     
-    fname = args["--save"]
+    fname = "diblock.data"
     open(fname, "w").write(final_string)
     print("Data file written in", fname)
 
